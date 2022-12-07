c28669ac9f76197e255b24b3303534d64aec61af
Mee <rafaleto@gmail.com>
2022-12-07 12:58:47 +0100

    feat: Train takes into account negative examples

--- test_1.py
+++ test_1.py
@@ -15,6 +15,12 @@ import torchvision
 import torchvision.transforms as transforms
 
 
+class Config:
+    batch_size    = 64
+    num_epochs    = 10
+    learning_rate = 0.001
+    vector_size   = 10
+
 
 # Define the convolutional neural network that takes a [1, 28, 28] image and outputs a [10] vector
 class Net(nn.Module):
@@ -27,7 +33,7 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1   = nn.Linear(16 * 4 * 4, 120)
         self.fc2   = nn.Linear(120, 84)
-        self.fc3   = nn.Linear(84, 10)
+        self.fc3   = nn.Linear(84, Config.vector_size)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -45,6 +51,14 @@ class CosineDistance(nn.Module):
     def forward(self, x, y):
         # return 1 - F.cosine_similarity(x, y, dim=1)
         return 1 - F.cosine_similarity(x, y, dim=1).mean()
+
+class CosineDistance_bi(nn.Module):
+    def __init__(self):
+        super(CosineDistance_bi, self).__init__()
+    
+    def forward(self, x, y, label):
+        a = F.cosine_similarity(x, y, dim=1)
+        return ((1-a)*label + a*(1-label)).mean()
 
 if __name__ == '__main__':
         
@@ -82,45 +96,57 @@ if __name__ == '__main__':
     class_pairs = []
     for i in range(10):
         class_pairs.append(deque())
-    
     for i in range(len(trainset)):
         class_pairs[trainset[i][1]].append(i)
-        # class_pairs[trainset[i][1]].append(trainset[i][1])
+        # class_pairs[trainset[i][1]].append(trainset[i][1]) # For debuggig
+
+    # print ("---------------")
+    # for i in class_pairs:
+    #     print(len(i))
 
     positive_examples = []
     for i in range(10):
-        for j in range(2000):
+        for j in range(1100):
             positive_examples.append((class_pairs[i].popleft(), class_pairs[i].popleft()))
 
+    # print ("---------------")
+    # for i in class_pairs:
+    #     print(len(i))
+
     negative_examples = []
-    for i in range(int(2000 / (10*10))):
+    while len(negative_examples) < 11000:
         for j in range(10):
             for k in range(10):
                 if j != k:
                     negative_examples.append((class_pairs[j].popleft(), class_pairs[k].popleft()))
+    negative_examples = negative_examples[:11000]
 
-    # Turn each index into an image
+    # print ("---------------")
+    # for i in class_pairs:
+    #     print(len(i))
+    # print ("---------------")
+
+    # Reshuffle time
+    np.random.shuffle(positive_examples)
+    np.random.shuffle(negative_examples)
+
+    # Indexes to images
     positive_examples_left  = torch.stack([trainset[i[0]][0] for i in positive_examples])
     positive_examples_right = torch.stack([trainset[i[1]][0] for i in positive_examples])
     negative_examples_left  = torch.stack([trainset[i[0]][0] for i in negative_examples])
     negative_examples_right = torch.stack([trainset[i[1]][0] for i in negative_examples])
 
-
+    # Debug (?)
     print("+L", len(positive_examples_left ))
     print("+R", len(positive_examples_right))
     print("-L", len(negative_examples_left ))
     print("-R", len(negative_examples_right))
 
-
-    # Batch the data into 64 chunks
-    positive_examples_left  = positive_examples_left .split(64)
-    positive_examples_right = positive_examples_right.split(64)
-    negative_examples_left  = negative_examples_left .split(64)
-    negative_examples_right = negative_examples_right.split(64)
-
-
-
-    p = 0
+    # Batch the data
+    positive_examples_left  = positive_examples_left .split(Config.batch_size)
+    positive_examples_right = positive_examples_right.split(Config.batch_size)
+    negative_examples_left  = negative_examples_left .split(Config.batch_size)
+    negative_examples_right = negative_examples_right.split(Config.batch_size)
 
     # Plot a grid
     if False:
@@ -133,42 +159,52 @@ if __name__ == '__main__':
         images, labels = dataiter.next()
         imshow(torchvision.utils.make_grid(images))
 
-
-
     # Train
     net         = Net()
     optimizer   = optim.Adam(net.parameters(), lr=0.001)
-    criterion_0 = nn.CrossEntropyLoss()
-    criterion_1 = CosineDistance()
+    # criterion_0 = nn.CrossEntropyLoss()
+    # criterion_1 = CosineDistance()
+    criterion_2 = CosineDistance_bi()
 
-    for epoch in range(2):
+    losses = []
+    for epoch in range(Config.num_epochs):
         running_loss = 0.0
-        for step, (a, b) in enumerate(zip(positive_examples_left, positive_examples_right)):
+        for step, (pos_l,pos_r, neg_l,neg_r) in enumerate(zip(positive_examples_left, positive_examples_right, negative_examples_left, negative_examples_right)):
             
+            combined_l = torch.cat((pos_l, neg_l))
+            combined_r = torch.cat((pos_r, neg_r))
+            labels     = torch.cat((torch.ones(combined_l.shape[0]//2), torch.zeros(combined_r.shape[0]//2)))
+
             # Train itself
-            output_a = net(a)
-            output_b = net(b)
+            output_a = net(combined_l)
+            output_b = net(combined_r)
             optimizer.zero_grad()
-            loss = criterion_1(output_a, output_b)
+            loss = criterion_2(output_a, output_b, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
 
             # Print loss
-            if step % 10 == 9:
-                print('[%d, %5d] loss: %.5f' % (epoch + 1, step + 1, running_loss / 100))
+            if step % 50 == 49:
+                losses.append(running_loss / 50)
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, step + 1, running_loss / 50))
                 running_loss = 0.0
 
-    with torch.no_grad():
-        for i in range(10):
-            output_a = net(positive_examples_left [i * 1000 // 64][0].view(-1, 1, 28, 28))
-            output_b = net(positive_examples_right[i * 1000 // 64][0].view(-1, 1, 28, 28))
-            print(criterion_1(output_a, output_b))
-        for i in range(10):
-            output_a = net(negative_examples_left [i][0].view(-1, 1, 28, 28))
-            output_b = net(negative_examples_right[i][0].view(-1, 1, 28, 28))
-            print(criterion_1(output_a, output_b))
+    # Plot losses
+    plt.plot(losses)
+    plt.show()
 
     p = 0
 
+    with torch.no_grad():
+        for i in range(10):
+            output_a = net(positive_examples_left [i * 1000 // Config.batch_size][0].view(-1, 1, 28, 28))
+            output_b = net(positive_examples_right[i * 1000 // Config.batch_size][0].view(-1, 1, 28, 28))
+            print(np.dot(output_a[0].numpy(), output_b[0].numpy()) / (np.linalg.norm(output_a[0].numpy()) * np.linalg.norm(output_b[0].numpy())))
+        print ("---------------")
+        for i in range(10):
+            output_a = net(negative_examples_left [i][0].view(-1, 1, 28, 28))
+            output_b = net(negative_examples_right[i][0].view(-1, 1, 28, 28))
+            print(np.dot(output_a[0].numpy(), output_b[0].numpy()) / (np.linalg.norm(output_a[0].numpy()) * np.linalg.norm(output_b[0].numpy())))
 
+    p = 0
